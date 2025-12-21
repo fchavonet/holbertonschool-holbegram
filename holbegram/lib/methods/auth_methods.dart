5541e4ba555c929dcc249f69214f71251a4cd1d5
Fabien Chavonet <fchavonet@mail.fr>
2025-12-21 10:01:17 +0100

    Add get user details method.

--- holbegram/lib/methods/auth_methods.dart
+++ holbegram/lib/methods/auth_methods.dart
@@ -73,4 +73,15 @@ class AuthMethode {
 
     return res;
   }
+
+  Future<Users> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+
+    DocumentSnapshot snap = await _firestore
+        .collection('users')
+        .doc(currentUser.uid)
+        .get();
+
+    return Users.fromSnap(snap);
+  }
 }
---
+++ holbegram/lib/providers/user_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+import '../methods/auth_methods.dart';
+
+class UserProvider with ChangeNotifier {
+  Users? _user;
+  final AuthMethode _authMethode = AuthMethode();
+
+  Users? get user => _user;
+
+  Future refreshUser() async {
+    Users user = await _authMethode.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
