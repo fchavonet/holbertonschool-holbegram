925e0bb29027003074bac3062e6cc52dbd2450d2
Fabien Chavonet <fchavonet@mail.fr>
2025-12-26 08:49:53 +0100

    Fix: user profile image upload.

--- holbegram/lib/methods/auth_methods.dart
+++ holbegram/lib/methods/auth_methods.dart
@@ -2,13 +2,28 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:http/http.dart' as http;
+import 'package:firebase_storage/firebase_storage.dart';
 
 import '../models/user.dart';
 
 class AuthMethode {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  /// Upload profile picture.
+  Future<String> _uploadProfileImage({
+    required String uid,
+    required Uint8List file,
+  }) async {
+    final Reference ref = _storage.ref().child("profilePics").child("$uid.jpg");
+
+    final UploadTask uploadTask = ref.putData(file);
+    final TaskSnapshot snap = await uploadTask;
+
+    final String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
 
   /// Login.
   Future<String> login({
@@ -23,7 +38,6 @@ class AuthMethode {
       }
 
       await _auth.signInWithEmailAndPassword(email: email, password: password);
-
       res = "success";
     } catch (err) {
       res = err.toString();
@@ -40,17 +54,31 @@ class AuthMethode {
     Uint8List? file,
   }) async {
     try {
-      UserCredential cred = await _auth.createUserWithEmailAndPassword(
+      if (email.isEmpty || password.isEmpty || username.isEmpty) {
+        return "Please fill all the fields...";
+      }
+
+      if (file == null) {
+        return "Please select a profile picture";
+      }
+
+      final UserCredential cred = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
 
-      Users user = Users(
-        uid: cred.user!.uid,
+      final String uid = cred.user!.uid;
+
+      // 1) Upload profile picture to Storage
+      final String photoUrl = await _uploadProfileImage(uid: uid, file: file);
+
+      // 2) Create Firestore user doc
+      final Users user = Users(
+        uid: uid,
         email: email,
         username: username,
         bio: '',
-        photoUrl: '',
+        photoUrl: photoUrl,
         followers: [],
         following: [],
         posts: [],
@@ -58,9 +86,11 @@ class AuthMethode {
         searchKey: username[0].toLowerCase(),
       );
 
-      await _firestore.collection('users').doc(user.uid).set(user.toJson());
+      await _firestore.collection('users').doc(uid).set(user.toJson());
 
       return "success";
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? e.toString();
     } catch (e) {
       return e.toString();
     }
@@ -72,9 +102,9 @@ class AuthMethode {
 
   /// Get user details
   Future<Users> getUserDetails() async {
-    User currentUser = _auth.currentUser!;
+    final User currentUser = _auth.currentUser!;
 
-    DocumentSnapshot snap = await _firestore
+    final DocumentSnapshot snap = await _firestore
         .collection('users')
         .doc(currentUser.uid)
         .get();
